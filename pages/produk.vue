007b25cd98e016056efa8a690e45505b399a777c
Sidiq <makbarsidiq@gmail.com>
2022-03-14 17:20:16 +0700

    add localhost

--- pages/produk.vue
+++ pages/produk.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     Produk :
-    <a href="https://sompo-qa/reservation/insurance/quotation">Beli Asuransi</a>
+    <ul>
+      <li>
+        <a href="https://sompo-qa/reservation/insurance/quotation">
+          Asuransi Sompo QA</a
+        >
+      </li>
+      <li>
+        <a href="http://localhost:44366/reservation/insurance/quotation">
+          Asuransi Sompo QA - localhost</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
